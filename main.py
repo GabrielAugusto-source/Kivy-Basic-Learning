3153ad14ee4a606bbd82c678015933d1db3d658c
Gabriel Augusto <gabriel.augusto.diegues@gmail.com>
2026-09-03 14:45:28 -0300

    Colors kivy

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
+import random
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.image import Image, AsyncImage
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 
-class MainApp(App):
+
+red = (1, 0, 0, 1)
+green = (0, 1, 0, 1)
+blue = (0, 0, 1, 1)
+yellow = (1, 1, 0, 1)
+
+
+class HBoxLayoutExample(App):
     def build(self):
-        
-        img = AsyncImage(
-            
-            size_hint=(None, None),
-            size=(200, 100),
-            pos_hint={'center_x': .5, 'center_y': .5}
-            
-        )
-        return Image(source="images/arthur.png")
+        self.title = "Kivy Colors"
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        colors = [red, green, blue, yellow]
+
+        for i in range(5):
+            btn = Button(text=f"Este é o botão #{i + 1}", background_color=random.choice(colors))
+            layout.add_widget(btn)
+
+        return layout
 
 if __name__ == "__main__":
-    MainApp().run()
+        app = HBoxLayoutExample()
+        app.run()
+        
+
+

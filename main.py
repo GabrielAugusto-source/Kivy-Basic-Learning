5faddaf17cec86f871264fd855491d9764915beb
Gabriel Augusto <gabriel.augusto.diegues@gmail.com>
2026-09-03 14:22:37 -0300

    Arthur em Imagem no kivy

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.image import Image, AsyncImage
 
 class MainApp(App):
     def build(self):
-        label = Label(text='Hello, Kivy!')
-        size_hint = (.5, .5)
-        pos_hint = {'center_x': .5, 'center_y': .5}
-    
-        return label
+        
+        img = AsyncImage(
+            
+            size_hint=(None, None),
+            size=(200, 100),
+            pos_hint={'center_x': .5, 'center_y': .5}
+            
+        )
+        return Image(source="images/arthur.png")
+
 if __name__ == "__main__":
-        app = MainApp()
-        app.run ()
+    MainApp().run()

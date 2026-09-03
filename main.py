06b808eb3cd4cbeaf2bb7f2de8ad36a4fc7ecd4b
Gabriel Augusto <gabriel.augusto.diegues@gmail.com>
2026-09-03 15:35:13 -0300

    Aprendizado básico sobre Kivy do Python

--- main.py
+++ main.py
@@ -1,32 +1,112 @@
-import random
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
+from database import DataBase
 
+class CreateAccountWindow(Screen):
+    namee = ObjectProperty(None)
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
 
-red = (1, 0, 0, 1)
-green = (0, 1, 0, 1)
-blue = (0, 0, 1, 1)
-yellow = (1, 1, 0, 1)
+    def submit(self):
+        if self.namee.text != "" and self.email.text != "" and self.email.text.count("@") == 1 and self.email.text.count(".") > 0:
+            if self.password.text != "":
+                db.add_user(self.email.text, self.password.text, self.namee.text)
+                self.reset()
+                sm.current = "login"
+            else:
+                invalidForm()
+        else:
+            invalidForm()
 
+    def login(self):
+        self.reset()
+        sm.current = "login"
 
-class HBoxLayoutExample(App):
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+        self.namee.text = ""
+
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginBtn(self):
+        if db.validate(self.email.text, self.password.text):
+            MainWindow.current = self.email.text
+            self.reset()
+            sm.current = "main"
+        else:
+            invalidLogin()
+
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+
+class MainWindow(Screen):
+    n = ObjectProperty(None)
+    created = ObjectProperty(None)
+    email = ObjectProperty(None)
+    current = ""
+
+    def logOut(self):
+        sm.current = "login"
+
+    def on_enter(self, *args):
+        password, name, created = db.get_user(self.current)
+        self.n.text = "Account Name: " + name
+        self.email.text = "Email: " + self.current
+        self.created.text = "Created On: " + created
+
+class WindowManager(ScreenManager):
+    pass
+
+def invalidLogin():
+    pop = Popup(
+        title='Invalid Login',
+        content=Label(text='Invalid username or password.'),
+        size_hint=(None, None), 
+        size=(400, 400)
+    )
+    pop.open()
+
+def invalidForm():
+    pop = Popup(
+        title='Invalid Form',
+        content=Label(text='Please fill in all inputs with valid information.'),
+        size_hint=(None, None), 
+        size=(400, 400)
+    )
+    pop.open()
+
+# Carrega o arquivo de interface do Kivy
+kv = Builder.load_file("my.kv")
+
+sm = WindowManager()
+db = DataBase("users.txt")
+
+screens = [
+    LoginWindow(name="login"), 
+    CreateAccountWindow(name="create"), 
+    MainWindow(name="main")
+]
+
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
+
+class MyMainApp(App):
     def build(self):
-        self.title = "Kivy Colors"
-        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
-        colors = [red, green, blue, yellow]
-
-        for i in range(5):
-            btn = Button(text=f"Este é o botão #{i + 1}", background_color=random.choice(colors))
-            layout.add_widget(btn)
-
-        return layout
+        return sm
 
 if __name__ == "__main__":
-        app = HBoxLayoutExample()
-        app.run()
-        
-
-
+    MyMainApp().run()
